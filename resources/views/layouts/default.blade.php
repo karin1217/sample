79dcd92b02cc2e700273dcebb74c4a1c964898b5
karin1217 <1946294910@qq.com>
2017-11-07 18:15:53 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title','Sample') - Laravel 入门教程</title>
+    <style>
+    h1{color:#DD4A68}
+    </style>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
